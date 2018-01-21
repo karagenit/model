c96276653efe9079fb9ee6837311d69e2f47958c
Caleb Smith <karagenit@outlook.com>
2018-01-21 13:33:52 -0500

    Update Tests

--- test/test-belir.rb
+++ test/test-belir.rb
@@ -4,10 +4,15 @@ require_relative '../lib/belir.rb'
 require 'test/unit'
 
 class TestBelir < Test::Unit::TestCase
-  def test_new_eqn
+  def test_eqn_one
     eq = Belir::Equation.new(:x, :y) { |y| y * 2 }
     assert_equal(eq.calculate(2), 4)
+    # assert_raise(ArgumentError) { eq.calculate(2, 2) }
+  end
+
+  def test_eqn_two
     eq = Belir::Equation.new(:x, :y, :z) { |y, z| y + z }
     assert_equal(eq.calculate(2, 3), 5)
+    # assert_raise(ArgumentError) { eq.calculate(2) }
   end
 end
